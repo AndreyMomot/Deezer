2cad0e8547d4e273b4938522d8f1acf8dcd73f16
Andrii Momot <momot.andrii@pm.bet>
2022-04-20 21:26:03 +0200

    feat: add basic project structure

--- Deezer/Modules/Home/HomeViewController.swift
+++ Deezer/Modules/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Deezer
 //
 //  Created by Andrii Momot on 20.04.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
-
