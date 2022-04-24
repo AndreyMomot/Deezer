07017b3709df49c0818feafbefa9080e8e0f699b
Andrii <Omg0Mom@gmail.com>
2022-04-24 12:29:35 +0200

    feat: handle custom errors (#5)
    Co-authored-by: Andrii Momot <momot.andrii@pm.bet>

--- Deezer/Helpers/ErrorHelper.swift
+++ Deezer/Helpers/ErrorHelper.swift
@@ -10,3 +10,10 @@ import Foundation
 enum AppError: String, Error {
     case defaultError = "Oooops. Something went wrong! Please try again later."
 }
+
+extension AppError: LocalizedError {
+  
+  var errorDescription: String? {
+    return rawValue
+  }
+}
